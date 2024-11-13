55bbbfbbe062f8d9f7f58c440d83ba4c41e39442
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-13 16:54:50 -0300

    Agregando archivos al  proyecto

---
+++ prueba.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source .env
+
+D=$DB
+U=$USER
+C=$CLAVE
+I=192.168.0.151
+CD="CREATE DATABASE $D;"
+CUS="CREATE USER $U@localhost IDENTIFIED BY $C;" 
+GUP="GRANT ALL ON $D.* to $U@$I IDENTIFIED BY $C WITH GRANT OPTION;"
+
+echo $CD >> init.sql
+echo $CUS >> init.sql
+echo $GUP >> init.sql
