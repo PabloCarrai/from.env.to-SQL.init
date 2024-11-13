2546e2cb435ddb5b3c03cb31a980138ed82287a4
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-13 17:10:10 -0300

    Ahora reviso si existe el archivo  y lo elimino porque sino voy a tener duplicado la data

--- from.env-to.Init.sql.sh
+++ from.env-to.Init.sql.sh
@@ -8,6 +8,11 @@ I=192.168.0.151
 CD="CREATE DATABASE $D;"
 CUS="CREATE USER $U@localhost IDENTIFIED BY $C;" 
 GUP="GRANT ALL ON $D.* to $U@$I IDENTIFIED BY $C WITH GRANT OPTION;"
+FI=init.sql
+
+if test -f "$FI"; then
+    rm $FI
+fi
 
 echo $CD >> init.sql
 echo $CUS >> init.sql
